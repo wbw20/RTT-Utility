fd340a7cddcd816f4974434c037cd64633a86473
William B. Wettersten <wbw20@case.edu>
2013-12-02 19:20:44 -0500

    we are building packets

--- main.py
+++ main.py
@@ -8,25 +8,29 @@ UDP_PROTOCOL_ID = 17 # RFC 768
 def checksum(packet):
   """Calculate the checksum of the combined UDP and IP pseudoheader according to RFC 768."""
 
-def to_b(ip):
-  """Convert an ip address string to a binary"""
-  return ''.join([bin(int(x)+256)[3:] for x in ip.split('.')])
+  sum = 0
+
+  for x in packet:
+    sum = sum + x
+    sum = sum & 0xffffffff
+
+  return sum
+
+def to_i(ip):
+  """Convert an ip address string to an int"""
+  return int(''.join([bin(int(x)+256)[3:] for x in ip.split('.')]))
 
 def create_packet(source, dest):
   """Creates a no-data UDP packet for the given source and destination."""
 
   # Pseudo header
   # source (32), destination (32), protocol (16), length (16)
-  pseudo = struct.pack('IIhh', source, dest, UDP_PROTOCOL_ID, 8)
+  pseudo = [to_i(source), to_i(dest), UDP_PROTOCOL_ID, 8]
 
-  print(checksum(pseudo))
-
-  # Return our actual checksum
-  # source (32), destination (32), length(16), checksum (16)
-  return struct.pack('IIhh', source, dest, 8, checksum(pseudo))
+  return [to_i(source), to_i(dest), 8, checksum(pseudo)]
 
 def ping(dest):
   print 'unused'
 
 if __name__ == '__main__':
-  print to_b('192.168.1.1')
+  print create_packet('192.168.1.1', '64.233.191.255')
